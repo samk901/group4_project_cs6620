5f000cd65e83d89c48a586e2a5d0a0f81c59e688
Brad <bradegan@gmail.com>
2022-03-30 17:35:26 -0400

    send_cmd

---
+++ deploy/boto_scripts/send_cmd.py
@@ -0,0 +1,22 @@
+import boto3
+import botocore
+import paramiko
+
+ec2 = boto3.resource('ec2')
+
+
+def send_cmd(ip, cmd):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    privkey = paramiko.RSAKey.from_private_key_file('final_project_key.pem')
+    ssh.connect(ip ,username='ec2-user',pkey=privkey)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    stdin.flush()
+    data = stdout.read().splitlines()
+    for line in data:
+        x = line.decode()
+        #print(line.decode())
+        #print(x,i)
+        ssh.close()
+
+send_cmd('54.145.13.62', 'mkdir test')
