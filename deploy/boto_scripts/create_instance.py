57e3d19afa180f62c0d284081d4e51ea0ac3c208
DJF <danielfinelli@Daniels-MacBook-Pro.local>
2022-03-30 11:14:26 +0200

    create instance basic

---
+++ deploy/boto_scripts/create_instance.py
@@ -0,0 +1,32 @@
+import boto3
+import botocore
+
+def create_instance():
+
+	try:
+		ec2 = boto3.resource('ec2')
+	except botocore.exceptions.ClientError as e:
+		print(e)
+
+	try:
+		instance = ec2.create_instances(
+		ImageId = 'ami-033b95fb8079dc481',
+		MinCoint = 1,
+		MaxCount = 1,
+		InstanceType = 't2.micro',
+		KeyName = 'lab2key',
+		TagSpecifications= [
+                    {
+                        'ResourceType': 'instance',
+                        'Tags': [
+                                    {
+                                        'Key': 'InstanceKey',
+                                        'Value': 'InstanceValue'
+                                    },
+                                ]
+                   },
+              	],
+		BlockDeviceMappings=[{"DeviceName": "/dev/xvda","Ebs" : { "VolumeSize" : 10 }}]
+		)
+	except: boto3core.exceptions.ClientError as e:
+		print(e)
