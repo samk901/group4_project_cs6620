e20d85761a6eb7a0ab29288e6bb3716ecbd7104e
Brad <bradegan@gmail.com>
2022-04-05 11:07:01 -0400

    added some changes to set up db instance

--- deploy/boto_scripts/create_key_pair.py
+++ deploy/boto_scripts/create_key_pair.py
@@ -3,7 +3,7 @@ import boto3
 def create_pem():
     try:
         ec2 = boto3.client('ec2')
-        keyname = 'final_project_' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=))
+        keyname = 'final_project_' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
         response = ec2.create_key_pair(KeyName=keyname)
         #print(response.keys())
         with open('./'+keyname+'.pem', 'w') as file:
