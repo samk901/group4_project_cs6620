28924485135b5631eaeb50299dde4e36bd6ae688
samk901 <samk90@gmail.com>
2022-04-20 11:51:44 -0400

    bug in nvm install

--- install_scripts/nvm_node_install.sh
+++ install_scripts/nvm_node_install.sh
@@ -11,6 +11,7 @@ INSTALL_NVM_VER=0.35.3
 
 echo "==> Ensuring .bashrc exists and is writable"
 touch ~/.bashrc
+. ~/.nvm/nvm.sh
 
 echo "==> INstalling node version manager (NVM). Version $INSTALL_NVM_VER"
 # Removes nvm if already installed
@@ -24,8 +25,8 @@ source ~/.bashrc
 
 # Allows user to use nvm without reopening terminal
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 echo "==> Installing node js version $INSTALL_NODE_VER"
 nvm install $INSTALL_NODE_VER
