31ffe5ed93765a1a1846e45950336f1f989c346c
samk901 <samk90@gmail.com>
2022-04-20 09:41:58 -0400

    updated install script, added Sample.env

--- install_scripts/nvm_node_install.sh
+++ install_scripts/nvm_node_install.sh
@@ -9,7 +9,7 @@ UIDIR=/tracker-ui
 INSTALL_NODE_VER=10.24.0
 INSTALL_NVM_VER=0.35.3
 
-echo "==> ENsuring .bashrc exists and is writable"
+echo "==> Ensuring .bashrc exists and is writable"
 touch ~/.bashrc
 
 echo "==> INstalling node version manager (NVM). Version $INSTALL_NVM_VER"
@@ -31,5 +31,9 @@ echo "==> Make this version system default"
 nvm alias default $INSTALL_NODE_VER
 nvm use default
 
+# Allows user to use nvm without reopening terminal
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 
