6578cc41287515ff4fb0fa3b0805a65834251d20
Jeff Taylor <taylor.je@husky.neu.edu>
2022-04-23 14:10:41 -0400

    Create sg accomodate load balancer

--- deploy/boto_scripts/create_and_setup_load_balancer.py
+++ deploy/boto_scripts/create_and_setup_load_balancer.py
@@ -107,46 +107,25 @@ def create_listener(target_group_arn, load_balancer_arn):
     else:
         return response
 
-#Whether in this script or a separate script, we need to initially create (or update) the security group associated with
+#Moving this to create_secuirty_group.py. Initially create the security group associated with
 # our EC2 instances to only allow incoming traffic from the load balancer.  Don't want to allow direct http traffic to ec2 instances
-def testing_secruity_group_with_traffic_only_allowed_from_load_balancer():
+def create_secruity_group_with_traffic_only_allowed_from_load_balancer():
+    #Method moved to create_security_group.py file
+    return True
 
-    try:
-        ec2 = boto3.resource('ec2')
-        sg = ec2.create_security_group(GroupName="testing_sg_group", Description='Testing123', TagSpecifications=[
-                                                                                                        {'ResourceType': 'security-group',
-                                                                                                         'Tags': [{'Key': 'SgTagKey', 'Value': 'SgTagVal'}]
-                                                                                                        }
-                                                                                                          ])
-        
-        sg.authorize_ingress(
-            IpPermissions=[{
-                'IpProtocol': 'tcp',
-                'FromPort': 80,
-                'ToPort': 80,
-                'UserIdGroupPairs': [{
-                    'GroupId': 'sg-0abe12d6651793059'
-                }]
-            }]
-        )
-
-    except botocore.exceptions.ClientError as e:
-        print(e)
-        pass
 
 def main():
 
-    # load_balancer_security_group = create_load_balancer_security_group('vpc-08a8476b32003e8d3') #Temporary testing with my VPC_ID, will use vpc_id we generate
-    # load_balancer = create_load_balancer(load_balancer_security_group.id, [
-    #     'subnet-073cd7a90757fd3a4', #Temporary using these two subnets, we can update based on our setup
-    #     'subnet-0e7ef3710998198bc',
-    # ]) 
-    # target_group = create_target_group('vpc-08a8476b32003e8d3') #Temporary testing with my VPC_ID, will use vpc_id we generate
-    # registered_target1 = register_ec2_instance_with_target_group(list(target_group.values())[0][0].get('TargetGroupArn'), 'i-008b7ba987ee8d6ea') #Temporary testing with a hard-coded instance ID, will use instance ID we generate
-    # registered_target2 = register_ec2_instance_with_target_group(list(target_group.values())[0][0].get('TargetGroupArn'), 'i-0ce79f06373a78937')
-    # registered_target3 = register_ec2_instance_with_target_group(list(target_group.values())[0][0].get('TargetGroupArn'), 'i-0e155ee770df0dbe5')
-    # listener = create_listener(list(target_group.values())[0][0].get('TargetGroupArn'), list(load_balancer.values())[0][0].get('LoadBalancerArn')) #There is probably a much simpler syntax to extract 'TargetGroupArn' and 'LoadBalancerArn'  
-    testing_secruity_group_with_traffic_only_allowed_from_load_balancer()
+    load_balancer_security_group = create_load_balancer_security_group('vpc-08a8476b32003e8d3') #Temporary testing with my VPC_ID, will use vpc_id we generate
+    load_balancer = create_load_balancer(load_balancer_security_group.id, [
+        'subnet-073cd7a90757fd3a4', #Temporary using these two subnets, we can update based on our setup
+        'subnet-0e7ef3710998198bc',
+    ]) 
+    target_group = create_target_group('vpc-08a8476b32003e8d3') #Temporary testing with my VPC_ID, will use vpc_id we generate
+    registered_target1 = register_ec2_instance_with_target_group(list(target_group.values())[0][0].get('TargetGroupArn'), 'i-008b7ba987ee8d6ea') #Temporary testing with a hard-coded instance ID, will use instance ID we generate
+    registered_target2 = register_ec2_instance_with_target_group(list(target_group.values())[0][0].get('TargetGroupArn'), 'i-0ce79f06373a78937')
+    registered_target3 = register_ec2_instance_with_target_group(list(target_group.values())[0][0].get('TargetGroupArn'), 'i-0e155ee770df0dbe5')
+    listener = create_listener(list(target_group.values())[0][0].get('TargetGroupArn'), list(load_balancer.values())[0][0].get('LoadBalancerArn')) #There is probably a much simpler syntax to extract 'TargetGroupArn' and 'LoadBalancerArn'  
 
 if __name__ == "__main__":
     main()
--- deploy/boto_scripts/create_security_group.py
+++ deploy/boto_scripts/create_security_group.py
@@ -1,6 +1,8 @@
 import boto3
 import botocore
 
+#See updated create_sg_method below this method which accomodates load balancer security group
+#I (Jeff) propose we use second method
 def create_sg():
     
     try:
@@ -50,4 +52,59 @@ def create_sg():
 
     # return sg
 
-#create_sg()
+#Method to create security group for web servers and application servers.  We can modify if we 
+#want different security groups for web vs. application servers.  
+#Same as above method, but updated such that HTTP inbound traffic is allowed allowed from load balancer
+def create_security_group(sg_name, sg_description, load_balancer_sg_id):
+    
+    try:
+        ec2 = boto3.resource('ec2')
+        sg = ec2.create_security_group(GroupName=sg_name, Description=sg_description, TagSpecifications=[
+                                                                                                        {'ResourceType': 'security-group',
+                                                                                                         'Tags': [{'Key': 'SgTagKey', 'Value': 'SgTagVal'}]
+                                                                                                        }
+                                                                                                          ])
+        sg.authorize_ingress(IpPermissions=[{
+                                            'IpProtocol': 'tcp',
+                                            'FromPort': 80,
+                                            'ToPort': 80,
+                                            'UserIdGroupPairs': [{
+                                                'GroupId': load_balancer_sg_id
+                                            }]
+                                            },
+                                            {'IpProtocol': 'TCP',
+                                            'FromPort': 22,
+                                            'ToPort': 22,
+                                            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+                                            },
+                                            {'IpProtocol': 'TCP',
+                                            'FromPort': 8000,
+                                            'ToPort': 8000,
+                                            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+                                            },
+                                            {'IpProtocol': 'TCP',
+                                            'FromPort': 27017,
+                                            'ToPort': 27017,
+                                            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+                                            },
+                                            {'IpProtocol': 'TCP',
+                                            'FromPort': 3000,
+                                            'ToPort': 3000,
+                                            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+                                            },
+                                            {'IpProtocol': 'TCP',
+                                            'FromPort': 443,
+                                            'ToPort': 443,
+                                            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+                                            }
+                                            ]
+
+                                )
+
+    except botocore.exceptions.ClientError as e:
+        print(e)
+        pass
+
+create_security_group('Server security group', 
+                      'Security group for web and application ec2 instances', 
+                      'sg-0abe12d6651793059')
