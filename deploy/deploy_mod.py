0ace8d662ae2e417aa9c803bd3eb99e2a9a25bc7
Jeff Taylor <taylor.je@husky.neu.edu>
2022-04-28 20:02:25 -0400

    Commented Out

--- deploy/deploy_mod.py
+++ deploy/deploy_mod.py
@@ -15,24 +15,24 @@ def deploy():
 
     # Create db instance
     instances = create_instance(keyname, num_instances=3)
-    db_ip = instances[0].public_ip_address
-    db_id = instances[0].instance_id
+    # db_ip = instances[0].public_ip_address
+    # db_id = instances[0].instance_id
 
-    # Create api instance
-    api_ip = instances[1].public_ip_address
-    api_id = instances[1].instance_id
+    # # Create api instance
+    # api_ip = instances[1].public_ip_address
+    # api_id = instances[1].instance_id
 
-    # Create ui instance
-    ui_ip = instances[2].public_ip_address
-    ui_id = instances[2].instance_id
+    # # Create ui instance
+    # ui_ip = instances[2].public_ip_address
+    # ui_id = instances[2].instance_id
 
-    create_db_server(key, db_ip)
-    print('Try connecting at mongodb://' + db_ip)
+    # create_db_server(key, db_ip)
+    # print('Try connecting at mongodb://' + db_ip)
 
-    create_api_server(key, db_ip, api_ip)
-    print('Try connecting at', 'http://' + api_ip + ':3000/graphql')
+    # create_api_server(key, db_ip, api_ip)
+    # print('Try connecting at', 'http://' + api_ip + ':3000/graphql')
 
-    create_ui_server(key, api_ip, ui_ip)
-    print('Try connecting at', 'http://' + ui_ip+':3000')
+    # create_ui_server(key, api_ip, ui_ip)
+    # print('Try connecting at', 'http://' + ui_ip+':3000')
 
 deploy()
