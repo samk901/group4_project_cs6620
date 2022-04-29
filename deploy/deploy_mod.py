07cb9fed042db63e3d20fef5850fc5303b06eb4f
Jeff Taylor <taylor.je@husky.neu.edu>
2022-04-28 22:28:03 -0400

    replaced hyphens

--- deploy/deploy_mod.py
+++ deploy/deploy_mod.py
@@ -17,10 +17,8 @@ def deploy():
 
     #Create load balancer security group.  This will be used for both API load balancer and UI load balancer
     load_balancer_security_group = create_load_balancer_security_group(vpc_id)
-    print("getting here1")
 
     ec2_instance_security_group = create_security_group('final_project', 'EC2 instance security group', load_balancer_security_group.id)
-    print("getting here2")
 
     # Create ssh key
     keyname = create_pem()
@@ -58,8 +56,8 @@ def deploy():
     load_balancer_api = create_load_balancer(load_balancer_security_group.id, [
         'subnet-073cd7a90757fd3a4', #TODO: Update to dynamic
         'subnet-0e7ef3710998198bc',
-    ], 'api_load_balancer')
-    target_group_api = create_target_group(vpc_id, 'api_load_balancer_target_group') 
+    ], 'api-load-balancer')
+    target_group_api = create_target_group(vpc_id, 'api-load-balancer-target-group') 
     registered_target1_api = register_ec2_instance_with_target_group(list(target_group_api.values())[0][0].get('TargetGroupArn'), api_id1) 
     registered_target2_api = register_ec2_instance_with_target_group(list(target_group_api.values())[0][0].get('TargetGroupArn'), api_id2)
     listener_api = create_listener(list(target_group_api.values())[0][0].get('TargetGroupArn'), list(load_balancer_api.values())[0][0].get('LoadBalancerArn'))
@@ -74,8 +72,8 @@ def deploy():
     load_balancer_ui = create_load_balancer(load_balancer_security_group.id, [
         'subnet-073cd7a90757fd3a4', #TODO: update to dynamic
         'subnet-0e7ef3710998198bc',
-    ], 'ui_load_balancer')
-    target_group_ui = create_target_group(vpc_id, 'ui_load_balancer_target_group') #Temporary testing with my VPC_ID, will use vpc_id we generate
+    ], 'ui-load-balancer')
+    target_group_ui = create_target_group(vpc_id, 'ui-load-balancer-target-group') #Temporary testing with my VPC_ID, will use vpc_id we generate
     registered_target1_ui = register_ec2_instance_with_target_group(list(target_group_ui.values())[0][0].get('TargetGroupArn'), ui_id1)
     registered_target2_ui = register_ec2_instance_with_target_group(list(target_group_ui.values())[0][0].get('TargetGroupArn'), ui_id2)
     listener_ui = create_listener(list(target_group_ui.values())[0][0].get('TargetGroupArn'), list(load_balancer_ui.values())[0][0].get('LoadBalancerArn'))
