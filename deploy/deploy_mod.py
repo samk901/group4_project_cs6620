e828e3a18cc3be66ad5af7d7190087dc578c951d
Jeff Taylor <taylor.je@husky.neu.edu>
2022-04-28 21:20:21 -0400

    Testing all

--- deploy/deploy_mod.py
+++ deploy/deploy_mod.py
@@ -15,38 +15,72 @@ def deploy():
 
     create_iam_policy()
 
-    # #Create load balancer security group.  This will be used for both API load balancer and UI load balancer
-    # load_balancer_security_group = create_load_balancer_security_group(vpc_id)
-    # print("getting here1")
+    #Create load balancer security group.  This will be used for both API load balancer and UI load balancer
+    load_balancer_security_group = create_load_balancer_security_group(vpc_id)
+    print("getting here1")
 
-    # ec2_instance_security_group = create_security_group('final_project', 'EC2 instance security group', load_balancer_security_group.id)
-    # print("getting here2")
+    ec2_instance_security_group = create_security_group('final_project', 'EC2 instance security group', load_balancer_security_group.id)
+    print("getting here2")
 
     # Create ssh key
     keyname = create_pem()
     key = keyname + '.pem'
     print('Created key', key)
 
-    # Create db instance
-    instances = create_instance(keyname, num_instances=3)
+    # Create 5 EC2 instaces (1 for database, 2 for API servers, 2 for UI servers)
+    instances = create_instance(keyname, num_instances=5)
+
+    #DB instance info
     db_ip = instances[0].public_ip_address
     db_id = instances[0].instance_id
 
-    #Api instances
-    api_ip = instances[1].public_ip_address
-    api_id = instances[1].instance_id
+    #Api instance info
+    api_ip1 = instances[1].public_ip_address
+    api_id1 = instances[1].instance_id
+    api_ip2 = instances[2].public_ip_address
+    api_id2 = instances[2].instance_id
 
-    # Create ui instance
-    ui_ip = instances[2].public_ip_address
-    ui_id = instances[2].instance_id
+    # UI instance info
+    ui_ip1 = instances[3].public_ip_address
+    ui_id1 = instances[3].instance_id
+    ui_ip2 = instances[4].public_ip_address
+    ui_id2 = instances[4].instance_id
 
-    # create_db_server(key, db_ip)
-    # print('Try connecting at mongodb://' + db_ip)
+    #Setup db server with mongo and initialize dummy data in database
+    create_db_server(key, db_ip)
+    print('Try connecting at mongodb://' + db_ip)
 
-    # create_api_server(key, db_ip, api_ip)
-    # print('Try connecting at', 'http://' + api_ip + ':3000/graphql')
+    #Create API servers and create/setup load balancer for api servers
+    create_api_server(key, db_ip, api_ip1)
+    create_api_server(key, db_ip, api_ip2)
+    # print('Try connecting at', 'http://' + api_ip1 + ':3000/graphql')
 
-    create_ui_server(key, api_ip, ui_ip)
-    print('Try connecting at', 'http://' + ui_ip+':3000')
+    load_balancer_api = create_load_balancer(load_balancer_security_group.id, [
+        'subnet-073cd7a90757fd3a4', #TODO: Double check how we will get the subnets to pass in here
+        'subnet-0e7ef3710998198bc',
+    ])
+    target_group_api = create_target_group(vpc_id) 
+    registered_target1_api = register_ec2_instance_with_target_group(list(target_group_api.values())[0][0].get('TargetGroupArn'), api_id1) 
+    registered_target2_api = register_ec2_instance_with_target_group(list(target_group_api.values())[0][0].get('TargetGroupArn'), api_id2)
+    listener_api = create_listener(list(target_group_api.values())[0][0].get('TargetGroupArn'), list(load_balancer_api.values())[0][0].get('LoadBalancerArn'))
+    load_balancer_api_dns = list(load_balancer_api.values())[0][0].get('DNSName')
+    #----------------------------------------------------------------------------------------------------------------------------------
+
+    #Create UI servers and create/setup load balancer for UI servers
+    create_ui_server(key, load_balancer_api_dns, ui_ip1)
+    create_ui_server(key, load_balancer_api_dns, ui_ip2)
+    # print('Try connecting at', 'http://' + ui_ip1 +':3000')
+
+    load_balancer_ui = create_load_balancer(load_balancer_security_group.id, [
+        'subnet-073cd7a90757fd3a4', #TODO: Double check how we will get the subnets to pass in here
+        'subnet-0e7ef3710998198bc',
+    ])
+    target_group_ui = create_target_group(vpc_id) #Temporary testing with my VPC_ID, will use vpc_id we generate
+    registered_target1_ui = register_ec2_instance_with_target_group(list(target_group_ui.values())[0][0].get('TargetGroupArn'), ui_id1) #TODO: Replace ec2 instance ID here with one we generate
+    registered_target2_ui = register_ec2_instance_with_target_group(list(target_group_ui.values())[0][0].get('TargetGroupArn'), ui_id2) #TODO: Replace ec2 instance ID here with one we generate
+    listener_ui = create_listener(list(target_group_ui.values())[0][0].get('TargetGroupArn'), list(load_balancer_ui.values())[0][0].get('LoadBalancerArn'))
+    load_balancer_ui_dns = list(load_balancer_ui.values())[0][0].get('DNSName')
+    
+    print("Visit the live site at " + load_balancer_ui_dns)
 
 deploy()
